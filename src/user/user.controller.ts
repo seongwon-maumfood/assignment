c84a234efb8e7bea5a5b64157da84c7008c6529f
sw9384 <sw9384@maumfood.co.kr>
2022-08-25 08:52:18 +0900

    DEL: 주석삭제

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -13,7 +13,6 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 
-//성능향상을위해서 fastify가 최고다(express의 약 2배)
 @Controller('user')
 export class UserController {
   constructor(private readonly UserService: UserService) {}

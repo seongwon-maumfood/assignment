b97a39c36972e78e6ac647d6c77f395a5e3ccf67
sw9384 <sw9384@maumfood.co.kr>
2022-08-26 07:18:08 +0900

    ADD: 게시글 수정 api 완료

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -12,32 +12,24 @@ import {
 import { Request, Response } from 'express';
 import { BoardService } from './board.service';
 import { CreatePostDto } from './dto/create-post.dto';
-import { UpdateBoardDto } from './dto/update-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Controller('board')
 export class BoardController {
   constructor(private readonly boardService: BoardService) {}
 
   @Post('/post')
-  async create(@Req() req: Request, @Body() createBoardDto:CreatePostDto) {
-    return this.boardService.create(req['user'],createBoardDto);
-  }
-
-  @Get()
-  findAll(@Req() req: Request, @Body() title:string) {
-    console.log(11111111, req.body);
-    console.log(2222222, req['user']);
-    return this.boardService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.boardService.findOne(+id);
+  async createPost(@Req() req: Request, @Body() createBoardDto: CreatePostDto) {
+    return this.boardService.createPost(req['user'], createBoardDto);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateBoardDto: UpdateBoardDto) {
-    return this.boardService.update(+id, updateBoardDto);
+  updatePost(
+    @Req() req: Request,
+    @Param('id') id: string,
+    @Body() updatePostDto: UpdatePostDto,
+  ) {
+    return this.boardService.updatePost(req['user'], +id, updatePostDto);
   }
 
   @Delete(':id')
--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -2,13 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { Response } from '../interface';
 import { CreatePostDto } from './dto/create-post.dto';
-import { UpdateBoardDto } from './dto/update-post.dto';
+import { UpdatePostDto } from './dto/update-post.dto';
 
 @Injectable()
 export class BoardService {
   constructor(private prisma: PrismaService) {}
 
-  async create(
+  async createPost(
     authorId: string,
     createPostDto: CreatePostDto,
   ): Promise<Response> {
@@ -28,10 +28,8 @@ export class BoardService {
             create: { name: x },
           });
 
-          await this.prisma.postsOnTags.upsert({
-            where: { postId_tagId: { postId: post.id, tagId: tag.id } },
-            update: {},
-            create: { postId: post.id, tagId: tag.id },
+          await this.prisma.postsOnTags.create({
+            data: { postId: post.id, tagId: tag.id },
           });
         }
       }
@@ -61,8 +59,55 @@ export class BoardService {
     return `This action returns a #${id} board`;
   }
 
-  update(id: number, updateBoardDto: UpdateBoardDto) {
-    return `This action updates a #${id} board`;
+  async updatePost(
+    authorId: string,
+    id: number,
+    updatePostDto: UpdatePostDto,
+  ): Promise<Response> {
+    const post = await this.prisma.post.findUnique({ where: { id: id } });
+
+    // 작성자 체크
+    if (authorId !== post.authorId) {
+      return {
+        success: false,
+        data: null,
+        code: '401',
+        message: '작성자만 수정할 수 있습니다.',
+      };
+    }
+
+    const updatedPost = await this.prisma.post.update({
+      where: { id: id },
+      data: {
+        title: updatePostDto.title,
+        content: updatePostDto.content,
+      },
+    });
+
+    // 해당 포스트의 기존 태그들 삭제
+    await this.prisma.postsOnTags.deleteMany({
+      where: { postId: post.id },
+    });
+
+    if (updatePostDto.tags.length) {
+      for (let x of updatePostDto.tags) {
+        const tag = await this.prisma.tag.upsert({
+          where: { name: x },
+          update: {},
+          create: { name: x },
+        });
+        await this.prisma.postsOnTags.create({
+          data: { postId: post.id, tagId: tag.id },
+        });
+      }
+    }
+
+    return {
+      success:true,
+      data: updatedPost,
+      code: '200',
+      message:  '게시글 수정 완료'
+    }
   }
 
   remove(id: number) {

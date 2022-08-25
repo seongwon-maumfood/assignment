5dc880844ecc1feaba425caddc58af0c08446125
sw9384 <sw9384@maumfood.co.kr>
2022-08-25 18:01:32 +0900

    ADD: jwt authorization 미들웨어 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,27 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestModule,
+  RequestMethod,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
+import { BoardModule } from './board/board.module';
+import { JwtMiddleWare } from './jwt.middleware';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, BoardModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, JwtService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(JwtMiddleWare).forRoutes({
+      path: 'board',
+      method: RequestMethod.ALL,
+    });
+  }
+}
---
+++ src/jwt.middleware.ts
@@ -0,0 +1,58 @@
+import { Injectable, NestMiddleware, Options } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+import { JwtService } from '@nestjs/jwt';
+import { UserController } from './user/user.controller';
+import { PrismaService } from './prisma/prisma.service';
+
+@Injectable()
+export class JwtMiddleWare implements NestMiddleware<Request, Response> {
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly prisma: PrismaService,
+  ) {}
+  async use(req: Request, res: Response, next: NextFunction) {
+    const { authorization } = req.headers;
+    if (!authorization) {
+      return;
+    }
+    const [type, value] = authorization.split(' ');
+    if (type !== 'Bearer') {
+      return {
+        success: false,
+        data: null,
+        code: '401',
+        message: '유효하지 않은 토큰입니다.',
+      };
+    }
+    if (!value) {
+        return {
+            success: false,
+            data: null,
+            code: '401',
+            message: '유효하지 않은 토큰입니다.',
+          };
+    }
+    try {
+      const username = this.jwtService.verify(value, { secret: 'sw123123' });
+      const user = await this.prisma.user.findUnique({ where: { username } });
+      if (!user) {
+        return {
+            success: false,
+            data: null,
+            code: '401',
+            message: '유효하지 않은 토큰입니다.',
+          };
+      }
+      req['user'] = user.id;
+    } catch (e) {
+      console.log(e);
+      return {
+        success: false,
+        data: null,
+        code: '500',
+        message: 'Internal server error.',
+      };
+    }
+    next();
+  }
+}

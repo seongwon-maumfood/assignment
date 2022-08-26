19d84cf381ef2dbf350a1f658b19f06969d711ce
sw9384 <sw9384@maumfood.co.kr>
2022-08-26 09:04:14 +0900

    ADD: 에러 핸들러 추가

--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -66,91 +66,111 @@ export class PostService {
     id: number,
     updatePostDto: UpdatePostDto,
   ): Promise<Response> {
-    const post = await this.prisma.post.findUnique({ where: { id: id } });
-    if (!post) {
-      return {
-        success: false,
-        data: null,
-        code: '404',
-        message: 'Not Found',
-      };
-    }
-
-    // 작성자 체크
-    if (authorId !== post.authorId) {
-      return {
-        success: false,
-        data: null,
-        code: '401',
-        message: '작성자만 수정할 수 있습니다.',
-      };
-    }
-
-    const updatedPost = await this.prisma.post.update({
-      where: { id: id },
-      data: {
-        title: updatePostDto.title,
-        content: updatePostDto.content,
-      },
-    });
-
-    // 해당 포스트의 기존 태그들 삭제
-    await this.prisma.postsOnTags.deleteMany({
-      where: { postId: post.id },
-    });
-
-    if (updatePostDto.tags.length) {
-      for (let x of updatePostDto.tags) {
-        const tag = await this.prisma.tag.upsert({
-          where: { name: x },
-          update: {},
-          create: { name: x },
-        });
-        await this.prisma.postsOnTags.create({
-          data: { postId: post.id, tagId: tag.id },
-        });
+    try {
+      const post = await this.prisma.post.findUnique({ where: { id: id } });
+      if (!post) {
+        return {
+          success: false,
+          data: null,
+          code: '404',
+          message: 'Not Found',
+        };
       }
-    }
 
-    return {
-      success: true,
-      data: updatedPost,
-      code: '200',
-      message: '게시글 수정 완료',
-    };
+      // 작성자 체크
+      if (authorId !== post.authorId) {
+        return {
+          success: false,
+          data: null,
+          code: '401',
+          message: '작성자만 수정할 수 있습니다.',
+        };
+      }
+
+      const updatedPost = await this.prisma.post.update({
+        where: { id: id },
+        data: {
+          title: updatePostDto.title,
+          content: updatePostDto.content,
+        },
+      });
+
+      // 해당 포스트의 기존 태그들 삭제
+      await this.prisma.postsOnTags.deleteMany({
+        where: { postId: post.id },
+      });
+
+      if (updatePostDto.tags.length) {
+        for (let x of updatePostDto.tags) {
+          const tag = await this.prisma.tag.upsert({
+            where: { name: x },
+            update: {},
+            create: { name: x },
+          });
+          await this.prisma.postsOnTags.create({
+            data: { postId: post.id, tagId: tag.id },
+          });
+        }
+      }
+
+      return {
+        success: true,
+        data: updatedPost,
+        code: '200',
+        message: '게시글 수정 완료',
+      };
+    } catch (e) {
+      console.log(e);
+      return {
+        success: false,
+        data: null,
+        code: '500',
+        message: 'Internal server error.',
+      };
+    }
   }
 
   // 게시글 삭제
   async deletePost(authorId: string, id: number): Promise<Response> {
-    const post = await this.prisma.post.findUnique({ where: { id } });
-    if (!post) {
+    try {
+      const post = await this.prisma.post.findUnique({ where: { id } });
+      if (!post) {
+        return {
+          success: false,
+          data: null,
+          code: '404',
+          message: 'Not Found',
+        };
+      }
+
+      // 작성자 체크
+      if (authorId !== post.authorId) {
+        return {
+          success: false,
+          data: null,
+          code: '401',
+          message: '작성자만 삭제 할 수 있습니다.',
+        };
+      }
+
+      await this.prisma.postsOnTags.deleteMany({
+        where: { postId: id },
+      });
+      await this.prisma.post.delete({ where: { id } });
+      return {
+        success: true,
+        data: null,
+        code: '200',
+        message: '게시글 삭제 완료',
+      };
+    } catch (e) {
+      console.log(e);
       return {
         success: false,
         data: null,
-        code: '404',
-        message: 'Not Found',
+        code: '500',
+        message: 'Internal server error.',
       };
     }
-
-    // 작성자 체크
-    if (authorId !== post.authorId) {
-      return {
-        success: false,
-        data: null,
-        code: '401',
-        message: '작성자만 삭제 할 수 있습니다.',
-      };
-    }
-
-    await this.prisma.postsOnTags.deleteMany({
-      where: { postId: id },
-    });
-    await this.prisma.post.delete({ where: { id } });
-    return {
-      success: true,
-      data: null,
-      code: '200',
-      message: '게시글 삭제 완료',
-    };
   }
 }
